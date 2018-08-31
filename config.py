31ad26ff3fb692ff6e376d4aeabfdecda77ae455
Soldier101 <aruso80@gmail.com>
2018-08-31 13:14:33 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config file, duh
 
 # Your branch, ex: https://api.github.com/repos/beestation/beestation-13
-downstream = "https://api.github.com/repos/you/your-branch"
+downstream = "https://api.github.com/repos/Soldier101/Soldier101-Station"
 
 # their branch; the branch you're mirroring FROM
-upstream = "https://api.github.com/repos/them/their-branch"
+upstream = "https://api.github.com/repos/tgstation/tgstation"
 
 # your (or the bot's) github info
 username = "you@example.com"  # email could also go here
@@ -13,7 +13,7 @@ password = "VerySecurePassword"  # password, duh
 
 # The bot's master code repo, if you change the bot's code, YOU MUST ADD YOUR OWN REPO HERE, 
 # Or you violate the license
-repo = "https://github.com/CthulhuOnIce/Buzz-Bot"
+repo = "https://github.com/arkadiusz123-pull-request-bot/Buzz-Bot"
 
 # toggle extra features
 make_Changelogs = True  # automatically generate a changelog based on the cl tags in pull requests. (WIP, not done yet)
